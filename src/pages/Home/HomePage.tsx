c75abe834f22aeb6bb81335ae37bf0d7c42669c9
“Sigurd” <sigurd.vilstrup@gmail.com>
2024-10-28 12:19:19 +0100

    adds input output card front page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material";
 import FlowlogCard from "../../components/FlowlogCard";
 import ImportExport from "../../components/ImportExport";
+import IntakeOutputCard from "../../components/IntakeOutputCard";
 
 const HomePage = () => {
   return (
     <Box sx={{ maxWidth: 600, margin: "auto", padding: 3 }}>
       <FlowlogCard />
       <ImportExport />
+      <IntakeOutputCard totalIntake={0} totalOutput={0} />
     </Box>
   );
 };
